0974ceff923fcabc618f0f1d35098a2056dddd80
Logan Anderson <landerson2@upei.ca>
2022-01-24 14:33:15 -0500

    feat: update tina

--- pages/_app.js
+++ pages/_app.js
@@ -1,68 +1,68 @@
-
-   
-import dynamic from 'next/dynamic'
-import { TinaEditProvider } from 'tinacms/dist/edit-state'
+import dynamic from "next/dynamic";
+import { TinaEditProvider } from "tinacms/dist/edit-state";
 import {
   createGlobalStyle,
   ThemeProvider,
   Preflight,
   ColorModeProvider,
   th,
-} from '@xstyled/styled-components'
+} from "@xstyled/styled-components";
 
-import { theme } from '../config/theme'
-import { TinaCloudCloudinaryMediaStore } from "next-tinacms-cloudinary";
+import { theme } from "../config/theme";
 const GlobalStyle = createGlobalStyle`
-  ${th('global')}
-`
+  ${th("global")}
+`;
 
-const TinaCMS = dynamic(() => import('tinacms'), { ssr: false })
+const TinaCMS = dynamic(() => import("tinacms"), { ssr: false });
 
+const branch = process.env.NEXT_PUBLIC_EDIT_BRANCH || "main";
+const apiURL =
+  process.env.NODE_ENV == "development"
+    ? "http://localhost:4001/graphql"
+    : `https://content.tinajs.io/content/${process.env.NEXT_PUBLIC_TINA_CLIENT_ID}/github/${branch}`;
 
 const App = ({ Component, pageProps }) => {
   return (
     <ThemeProvider theme={theme}>
-    <ColorModeProvider>
-      <Preflight />
-      <GlobalStyle />
-      <TinaEditProvider
-        editMode={
-          <TinaCMS
-            clientId={process.env.NEXT_PUBLIC_TINA_CLIENT_ID}
-            branch={process.env.NEXT_PUBLIC_EDIT_BRANCH}
-            mediaStore={TinaCloudCloudinaryMediaStore}
-            isLocalClient={Boolean(
-              Number(process.env.NEXT_PUBLIC_USE_LOCAL_CLIENT ?? true)
-            )}
-            documentCreatorCallback={{
-              /**
-               * After a new document is created, redirect to its location
-               */
-              onNewDocument: ({ collection: { slug }, breadcrumbs }) => {
-                const relativeUrl = `/${slug}/${breadcrumbs.join("/")}`;
-                return (window.location.href = relativeUrl);
-              },
-              /**
-               * Only allows documents to be created to the `Blog Posts` Collection
-               */
-              filterCollections: (options) => {
-                return options.filter(
-                  (option) => option.label === "Documentation"
-                );
-              },
-            }}
-            
-            {...pageProps}
-          >
-            {(livePageProps) => <Component {...livePageProps} />}
-          </TinaCMS>
-        }
-      >
-        <Component {...pageProps} />
-      </TinaEditProvider>
+      <ColorModeProvider>
+        <Preflight />
+        <GlobalStyle />
+        <TinaEditProvider
+          editMode={
+            <TinaCMS
+              mediaStore={async () => {
+                const pack = await import("next-tinacms-cloudinary");
+                return pack.TinaCloudCloudinaryMediaStore;
+              }}
+              apiURL={apiURL}
+              documentCreatorCallback={{
+                /**
+                 * After a new document is created, redirect to its location
+                 */
+                onNewDocument: ({ collection: { slug }, breadcrumbs }) => {
+                  const relativeUrl = `/${slug}/${breadcrumbs.join("/")}`;
+                  return (window.location.href = relativeUrl);
+                },
+                /**
+                 * Only allows documents to be created to the `Blog Posts` Collection
+                 */
+                filterCollections: (options) => {
+                  return options.filter(
+                    (option) => option.label === "Documentation"
+                  );
+                },
+              }}
+              {...pageProps}
+            >
+              {(livePageProps) => <Component {...livePageProps} />}
+            </TinaCMS>
+          }
+        >
+          <Component {...pageProps} />
+        </TinaEditProvider>
       </ColorModeProvider>
     </ThemeProvider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
